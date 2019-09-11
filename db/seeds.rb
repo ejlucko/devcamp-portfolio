130c2affb0eec33bbc41dc662e69e4e0dd460492
Eric Lucko <eric.lucko@arcadiasolutions.com>
2019-09-11 15:31:34 -0400

    added new and edit buttons for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts "5 skills created"
 
 9.times do |portfolio_item|
 	Portfolio.create!(
-		title: "Portfolio title #{portfolio_item}",
+		title: "Portfolio title: #{portfolio_item}",
 		subtitle: "My great service",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 		main_image: "http://placehold.it/600x400",
